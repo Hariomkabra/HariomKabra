7306add4c8cba96c5559508672567c151631de51
Hariom Kabra <143120733+Hariomkabra@users.noreply.github.com>
2025-09-08 19:28:49 +0530

    updated

--- header_toggle.js
+++ header_toggle.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // JavaScript to Toggle Hamburger Menu
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
@@ -10,15 +10,3 @@ hamburger.addEventListener('click', () => {
 
 
 
-=======
-// JavaScript to Toggle Hamburger Menu
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navLinks.classList.toggle('active');
-});
-
-
->>>>>>> 22e74cacc5d03e56619b10547d1c7af770ca09d2
